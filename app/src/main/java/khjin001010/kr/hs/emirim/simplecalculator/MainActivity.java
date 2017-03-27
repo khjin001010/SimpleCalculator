2bad18839bf861c971aead39dc8adce70caa1a57
khjin001010 <khjin001010@naver.com>
2017-03-27 21:00:25 +0900

    #2 add

--- app/src/main/java/khjin001010/kr/hs/emirim/simplecalculator/MainActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/simplecalculator/MainActivity.java
@@ -1,13 +1,69 @@
 package khjin001010.kr.hs.emirim.simplecalculator;
 
+import android.net.ParseException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit_1;
+    EditText edit_2;
+    TextView text_result;
+    int input_1;
+    int input_2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit_1 = (EditText) findViewById(R.id.edit_1);
+        edit_2 = (EditText)findViewById(R.id.edit_2);
+
+        Button btn_add = (Button) findViewById(R.id.btn_add);
+        Button btn_sub = (Button) findViewById(R.id.btn_sub);
+        Button btn_mul = (Button) findViewById(R.id.btn_mul);
+        Button btn_div = (Button) findViewById(R.id.btn_div);
+
+        text_result = (TextView)findViewById(R.id.text_result);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input_1 = Integer.parseInt(edit_1.getText().toString());
+                input_2 = Integer.parseInt(edit_2.getText().toString());
+                text_result.setText("Result : "+(input_1+input_2));
+            }
+        });
+
+        btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input_1 = Integer.parseInt(edit_1.getText().toString());
+                input_2 = Integer.parseInt(edit_2.getText().toString());
+                text_result.setText("Result : "+ (input_1-input_2));
+            }
+        });
+
+        btn_mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input_1 = Integer.parseInt(edit_1.getText().toString());
+                input_2 = Integer.parseInt(edit_2.getText().toString());
+                text_result.setText("Result : "+ (input_1*input_2));
+            }
+        });
+
+        btn_div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input_1 = Integer.parseInt(edit_1.getText().toString());
+                input_2 = Integer.parseInt(edit_2.getText().toString());
+                text_result.setText("Result : "+ (input_1/input_2));
+            }
+        });
     }
 }
