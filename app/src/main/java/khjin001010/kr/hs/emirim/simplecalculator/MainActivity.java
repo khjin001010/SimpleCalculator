76d3a4a624cf23e5edbe44a277f4489ff145ce99
khjin001010 <khjin001010@naver.com>
2017-03-28 19:52:26 +0900

    #2 implements listener_switch_case

--- app/src/main/java/khjin001010/kr/hs/emirim/simplecalculator/MainActivity.java
+++ app/src/main/java/khjin001010/kr/hs/emirim/simplecalculator/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     EditText edit_1;
     EditText edit_2;
     TextView text_result;
@@ -29,8 +29,12 @@ public class MainActivity extends AppCompatActivity {
         Button btn_div = (Button) findViewById(R.id.btn_div);
 
         text_result = (TextView)findViewById(R.id.text_result);
+        btn_add.setOnClickListener(this);
+        btn_sub.setOnClickListener(this);
+        btn_mul.setOnClickListener(this);
+        btn_div.setOnClickListener(this);
 
-        btn_add.setOnClickListener(new View.OnClickListener() {
+       /* btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 input_1 = Integer.parseInt(edit_1.getText().toString());
@@ -64,6 +68,33 @@ public class MainActivity extends AppCompatActivity {
                 input_2 = Integer.parseInt(edit_2.getText().toString());
                 text_result.setText("Result : "+ (input_1/input_2));
             }
-        });
+        });*/
+    }
+
+    @Override
+    public void onClick(View view) {
+        input_1 = Integer.parseInt(edit_1.getText().toString());
+        input_2 = Integer.parseInt(edit_2.getText().toString());
+        int result = 0;
+
+        switch(view.getId()) {
+            case R.id.btn_add:
+                result = input_1 + input_2;
+                break;
+            case R.id.btn_sub:
+                result = input_1 - input_2;
+                break;
+            case R.id.btn_mul:
+                result = input_1 * input_2;
+                break;
+            case R.id.btn_div:
+                result = input_1 / input_2;
+                break;
+            default:
+                result = 0;
+        }
+
+        text_result.setText("Result : "+ result);
+
     }
 }
